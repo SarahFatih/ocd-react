80c8dadebc92d02b128d363de54571b7e592fb5a
SarahFatih <sara.fatih@netlight.com>
2020-01-09 15:35:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignInPage from './components/pages/authentication/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/authentication/SignIn.js
@@ -0,0 +1,44 @@
+import { auth, firebase, firebaseui } from '../../../Firebase/firebase';
+import React, { useEffect } from 'react';
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const SignInPage = () => {
+  useEffect(() => {
+    // Initialize the FirebaseUI Widget using Firebase.
+    var uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+        uiShown: function () {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      signInSuccessUrl: 'home',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ]
+    };
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });
+
+  return (
+    <div>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
+    </div>
+  );
+}
+
+export default SignInPage;
+
